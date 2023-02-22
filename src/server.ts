bc7cdf9a2d6cca80cf1d3b6c777c1c9e966db804
Leticia Gonçalves <leticiagoncalves.tech@gmail.com>
2023-02-22 10:06:45 -0300

    adding seeder, post and getday routes

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,20 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './routes'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
 // app.register(cors, {
 //   origin: ['http://localhost:3000'],
 // })
 
 app.register(cors)
-
-app.get('/', async () => {
-  const habits = await prisma.habit.findMany()
-
-  return habits
-})
+app.register(appRoutes)
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('server running')
+    console.log('HTTP Server running!')
   })
